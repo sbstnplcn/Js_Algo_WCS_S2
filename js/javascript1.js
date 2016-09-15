c13873f3707d77a63c6517d16eb316ccb53e2c9d
Sebastien <sbstnplcn@gmail.com>
2016-09-15 10:52:48 +0200

    fix infobox w/ markers

--- js/javascript1.js
+++ js/javascript1.js
@@ -192,37 +192,37 @@ $("#QbuttonAjax").on("click", function() {
     $('#Qajax').toggleClass('hide')
 })
 
-})
-// Google API key : AIzaSyAx5FDqSbdMpU6pOR6B8hTide4bKWY-Fn4
-
-function initMap() {
-    let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 6,
-        center: new google.maps.LatLng(44.8333,-0.5667)
-    });
-    let geocoder = new google.maps.Geocoder();
-    geocodeAddress(geocoder, map);
-}
+let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: new google.maps.LatLng(45,2.6)
+});
+let geocoder = new google.maps.Geocoder();
+geocodeAddress(geocoder, map);
 let address
 let contentString
-let marker
+
 function geocodeAddress(geocoder, resultsMap) {
     $.getJSON("ajax.json", function(json) {
         json.forEach(function(val) {
             address = val.ville
             contentString = `${val.nom} ${val.Prenom}`
-            var infowindow = new google.maps.InfoWindow({
+            let infowindow = new google.maps.InfoWindow({
                 content: contentString
-              });
+            })
             geocoder.geocode({'address': address}, function(results) {
-              marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location
-              })
-              marker.addListener('click', function() {
-                  infowindow.open(map, marker);
-                });
+                let marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location
+                })
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker)
+                    $(this).toggleClass('hide')
+                })
             })
         })
     })
 }
+
+
+})
+// Google API key : AIzaSyAx5FDqSbdMpU6pOR6B8hTide4bKWY-Fn4
